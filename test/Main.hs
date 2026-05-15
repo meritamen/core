d23fb1eb3e24365cdcbd2b5f1c43ca1f0781bd7e
Mrjtjmn <meritamen@sdf.org>
2026-05-15 15:55:32 +0800

    Add CI for GHC 9.12.4

--- test/Main.hs
+++ test/Main.hs
@@ -3,6 +3,7 @@
 module Main (main) where
 
 import           Control.Monad.State.Strict (evalState)
+import           Data.Bifunctor
 import           Data.Text                  (Text)
 import qualified Data.Text                  as T
 import qualified Data.Text.IO               as TIO
@@ -15,25 +16,23 @@ import Core.LambdaLifting
 import Core.Machine
 import Core.Parser
 
-testPath :: String
-testPath = "test/examplePrograms/"
+testDirectory :: FilePath
+testDirectory = "./test/examplePrograms/"
 
-testPrograms :: [(String, Text)]
-testPrograms = addPrefix testPath <$>
-              [("arithmetic1.core", "7")
-              , ("arithmetic2.core", "1024")
-              , ("casejump.core", "2")
-              , ("factorial.core", "3628800")
-              , ("fibonazzi.core", "377")
-              , ("gcd.core", "12")
-              , ("letrec.core", "1")
-              , ("undefined.core", "123")
-              , ("logical1.core", "Pack{2,0}")
-              , ("logical2.core", "Pack{2,0}")
-              , ("lambda1.core", "5")
-              , ("lambda2.core", "2")]
-  where
-    addPrefix prefix (path, result) = (prefix <> path, result)
+testPairs :: [(FilePath, Text)]
+testPairs =   first (testDirectory <>)
+  <$> [("arithmetic1.core", "7")
+      , ("arithmetic2.core", "1024")
+      , ("casejump.core", "2")
+      , ("factorial.core", "3628800")
+      , ("fibonazzi.core", "377")
+      , ("gcd.core", "12")
+      , ("letrec.core", "1")
+      , ("undefined.core", "123")
+      , ("logical1.core", "Pack{2,0}")
+      , ("logical2.core", "Pack{2,0}")
+      , ("lambda1.core", "5")
+      , ("lambda2.core", "2")]
 
 extractResult :: [GmState] -> Text
 extractResult states =  gmOutput $ last states
@@ -49,6 +48,6 @@ main :: IO ()
 main = do
   tl <- mapM (\(file, expected) -> do
                  prog <- TIO.readFile file
-                 return $ TestLabel file (doTest prog expected)) testPrograms
+                 return $ TestLabel file (doTest prog expected)) testPairs
   result <- runTestTT (TestList tl)
   if errors result > 0 || failures result > 0 then Exit.exitFailure else Exit.exitSuccess
