15690098ed1f53ab6a1c109f0e3d3ade5e55758d
Mrjtjmn <meritamen@sdf.org>
2026-07-01 21:37:31 +0800

    Replace getArgs with optparse-applicative for cli parsing

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,26 @@ module Main where
 
 import Core
 import qualified Data.Text.IO as TIO
-import System.Environment (getArgs)
+import Options.Applicative
+
+data Option = Option { fileName :: FilePath }
+
+parser :: Parser Option
+parser = Option <$> strArgument
+  ( metavar "FILE"
+   <> help "Path to the input file containing the program"
+  )
+
+cliParser :: ParserInfo Option
+cliParser = info
+  (parser <**> helper)
+  ( fullDesc
+   <> progDesc "Dump G-machine execution trace from a given file"
+   <> header "core - G-machine implementation of lazy programming language Core"
+  )
 
 main :: IO ()
 main = do
-  fileName <- (!! 0) <$> getArgs
+  fileName <- fileName <$> execParser cliParser
   program <- TIO.readFile fileName
   TIO.putStrLn . dumpCoreTrace $ program
